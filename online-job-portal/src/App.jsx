83736a01855a89daab0c9bee2ba9dfcf0efbaa8d
architbatham <71764919+architbatham@users.noreply.github.com>
2023-11-12 08:18:20 +0530

    first commit

---
+++ online-job-portal/src/App.jsx
@@ -0,0 +1,14 @@
+
+
+function App() {
+  
+  return (
+    <>
+      <div>
+        <h1 className='bg-blue-500 text-black text-3xl'>Online Job Portal</h1>
+      </div>
+    </>
+  )
+}
+
+export default App
