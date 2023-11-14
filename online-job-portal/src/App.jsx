e9a70eee1919b394dbe12540e33a2820b6d8f865
architbatham <71764919+architbatham@users.noreply.github.com>
2023-11-14 17:27:54 +0530

    Header Navbar Search

--- online-job-portal/src/App.jsx
+++ online-job-portal/src/App.jsx
@@ -1,14 +1,17 @@
 import Navbar from "./components/Navbar"
-import Header from "./components/Header"
-import searchBar from "./components/SearchBar"
+import Header from "./components/header"
+import SearchBar from "./components/SearchBar"
 
 function App() {
   
   return (
+    
       <div>
         <Navbar />
         <Header />
+        <SearchBar/>  
       </div>
+
   )
 }
 
